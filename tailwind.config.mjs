00b5a8c9f0d9a9a8b0dc22de204ae249611db13e
Karan <email@karanshergill.com>
2024-08-13 14:12:28 +0530

    fixed gradient

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,16 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				'comfortaa':['Comfortaa', 'sans-serif'] 
+				'heading': ['"Russo One"', 'system-ui', 'sans-serif'],
+				'sub-heading': ['"Belanosima"', 'system-ui', 'sans-serif'],
+				'description': ['"Space Grotesk"', 'system-ui', 'sans-serif'],
+				'paragraph': ['"Rajdhani"', 'system-ui', 'sans-serif'],
+				'footer' : ['"Exo 2"', 'system-ui', 'sans-serif']
 			},
+			backgroundImage: {
+				'img-home': "url('/src/assets/images/bg-home.png')",
+				'gradient-hero': "linear-gradient(to right, #facc15 0%, #ec4899 30%, #4f46e5 40%, #0ea5e9 60%, #10b981 60%, #4f46e5 90%, #ec4899 100%)"
+			}
 		},
 	},
 	plugins: [],
