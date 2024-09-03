fb93180f53d54111c6370494b9141d5be68df4d6
Karan <email@karanshergill.com>
2024-09-04 02:38:22 +0530

    major fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
-
 export default defineConfig({
-    output: "hybrid",
   integrations: [
     tailwind(),
     react()
